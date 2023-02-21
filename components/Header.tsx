628d5cf68e9ee6073b2850e236d17b1c51b3c84f
Richard <richardebo.bentil@gmail.com>
2023-02-21 20:47:57 +0000

    fix header menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,57 +1,26 @@
 import React from "react";
-import { MdPerson, MdNotifications } from "react-icons/md";
+import { MdPerson, MdNotifications, MdShoppingBasket } from "react-icons/md";
 //menus: notifications, profile
 function Header() {
 	return (
 		<header>
 			<ul className="nav justify-content-center shadow-sm bg-white py-1">
-				<li className="nav-item me-auto">
+				<li className="nav-item">
 					<a className="nav-link text-dark" href="#">
 						<h5 className="mb-0">Food joint</h5>
 					</a>
 				</li>
-				<li className="nav-item dropdown d-none d-lg-block ms-auto">
-					<a
-						className="nav-link dropdown-toggle text-dark"
-						href="#"
-						role="button"
-						data-bs-toggle="dropdown"
-						aria-expanded="false">
-						Menu
-					</a>
-					<ul className="dropdown-menu dropdown-menu-end ">
-						<li>
-							<a className="dropdown-item" href="#">
-								Dishes
-							</a>
-						</li>
-						<li>
-							<a className="dropdown-item" href="#">
-								Vendors
-							</a>
-						</li>
-						<li>
-							<hr className="dropdown-divider" />
-						</li>
-						<li>
-							<a className="dropdown-item" href="#">
-								Cart
-							</a>
-						</li>
-						<li>
-							<a className="dropdown-item" href="#">
-								Orders
-							</a>
-						</li>
-					</ul>
-				</li>
-
-				<li className="nav-item ms-auto ms-md-0 me-1">
+				<li className="nav-item d-none d-lg-block me-3">
 					<a className="nav-link px-0 text-dark" href="#">
-						<MdNotifications size={20} />
+						Dishes
 					</a>
 				</li>
-				<li className="nav-item dropdown">
+				<li className="nav-item d-none d-lg-block me-3">
+					<a className="nav-link px-0 text-dark" href="#">
+						Vendors
+					</a>
+				</li>
+				<li className="nav-item dropdown ms-auto me-1">
 					<a
 						className="nav-link text-dark"
 						href="#"
@@ -61,6 +30,11 @@ function Header() {
 						<MdPerson size={20} />
 					</a>
 					<ul className="dropdown-menu dropdown-menu-end border-0">
+						<li className="d-none d-lg-block ">
+							<a className="dropdown-item" href="#">
+								Orders
+							</a>
+						</li>
 						<li>
 							<a className="dropdown-item" href="#">
 								Profile
@@ -75,6 +49,11 @@ function Header() {
 							</a>
 						</li>
 					</ul>
+				</li>
+				<li className="nav-item d-none d-lg-block">
+					<a className="nav-link text-dark ps-2" href="#">
+						<MdShoppingBasket size={20} />
+					</a>
 				</li>
 			</ul>
 		</header>
--- pages/dishes/index.tsx
+++ pages/dishes/index.tsx
@@ -1,13 +1,14 @@
 import FoodCard from '@/components/FoodCard'
-import React from 'react'
-import {foods} from "@/foods.json"
+import React, { useState } from 'react'
+import items from "@/foods.json"
 
 function Dishes() {
+	const { foods }: any = items;
   return (
 		<>
 			<h6 className='mb-1'>Popular dishes</h6>
 			<div className="row my-3">
-				{foods.map((food) => (
+				{foods.map((food: any ) => (
 					<div key={food.id} className="col-md-6 col-lg-4 mb-3">
 						<FoodCard food={food} />
 					</div>
